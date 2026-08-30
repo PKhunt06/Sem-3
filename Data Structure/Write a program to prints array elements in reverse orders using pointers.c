6e4bd34c99534f70bdd7c693c0f0c58ea4fe8b95
Khunt Parikshit <163092842+PKhunt06@users.noreply.github.com>
2026-08-30 16:54:45 +0530

    Add program to print array elements in reverse order using pointers
    This program takes an array of integers as input and prints the elements in reverse order using pointers.

---
+++ Data Structure/Write a program to prints array elements in reverse orders using pointers.c
@@ -0,0 +1,31 @@
+// P-2-A  Write a program to prints array elements in reverse orders using pointers.
+
+#include <stdio.h>
+
+int main()
+{
+    int arr[100], n, i;
+    int *ptr;
+
+    printf("Enter the number of elements: ");
+    scanf("%d", &n);
+
+    printf("Enter %d elements:\n", n);
+
+    for(i = 0; i < n; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+
+    ptr = &arr[n - 1];
+
+    printf("\nArray elements in reverse order are:\n");
+
+    for(i = n - 1; i >= 0; i--)
+    {
+        printf("%d ", *ptr);
+        ptr--;
+    }
+
+    return 0;
+}
